5517e29539783cb6ffb692c27e12a740dea9e663
Yasith Fernando <thekindofme@gmail.com>
2018-02-05 10:23:50 +1100

    Programatacally require all files in lib

--- zen_search.rb
+++ zen_search.rb
@@ -1,12 +1,4 @@
-require_relative 'lib/json_parse_error'
-require_relative 'lib/cli'
-require_relative 'lib/workflow_state_machine'
-require_relative 'lib/database'
-require_relative 'lib/category_table'
-require_relative 'lib/searchable_fields_parser'
-require_relative 'lib/streamed_json_file_searcher'
-require_relative 'lib/populate_user_attributes'
-require_relative 'lib/populate_ticket_attributes'
+Dir[File.join(File.dirname(__FILE__), 'lib', '*.rb')].each {|file| require file }
 
 database = Database.new(data_sources: {
   users: 'data/users.json',
